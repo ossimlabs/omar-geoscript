47395ec85ff26d9baba7c739627dd6c1577e22dc
Scott Bortman <scott.bortman@gmail.com>
2017-06-08 15:40:40 -0400

    Added some utility methods to help break wfs dependency on this module

--- plugins/omar-geoscript-plugin/grails-app/services/omar/geoscript/GeoscriptService.groovy
+++ plugins/omar-geoscript-plugin/grails-app/services/omar/geoscript/GeoscriptService.groovy
@@ -2,16 +2,23 @@ package omar.geoscript
 
 import geoscript.filter.Function
 import geoscript.geom.GeometryCollection
+import geoscript.layer.io.CsvWriter
 import geoscript.workspace.Workspace
-import grails.transaction.Transactional
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovy.xml.StreamingMarkupBuilder
 import org.geotools.data.DataStoreFinder
 import org.geotools.factory.CommonFactoryFinder
 import org.opengis.filter.capability.FunctionName
 import org.springframework.beans.factory.InitializingBean
 
+import grails.transaction.Transactional
+
 @Transactional( readOnly = true )
 class GeoscriptService implements InitializingBean
 {
+  def grailsLinkGenerator
+
   def parseOptions(def wfsParams)
   {
     def wfsParamNames = [
@@ -30,10 +37,11 @@ class GeoscriptService implements InitializingBean
           options['start'] = wfsParams[wfsParamName]
           break
         case 'propertyName':
-          def fields =  wfsParams[wfsParamName]?.split( ',' )?.collect {
+          def fields = wfsParams[wfsParamName]?.split( ',' )?.collect {
             it.split( ':' )?.last()
           } as List<String>
-          if ( fields && ! fields?.isEmpty() && fields?.every { it } ) {
+          if ( fields && !fields?.isEmpty() && fields?.every { it } )
+          {
             // println "FIELDS: ${fields.size()}"
             options['fields'] = fields
           }
@@ -43,11 +51,11 @@ class GeoscriptService implements InitializingBean
           {
             options['sort'] = wfsParams[wfsParamName].split( ',' )?.collect {
               def props = [it] as List
-              if(it.contains(" "))
+              if ( it.contains( " " ) )
               {
                 props = it.split( ' ' ) as List
               }
-              else if(it.contains("+"))
+              else if ( it.contains( "+" ) )
               {
                 props = it.split( "\\+" ) as List
               }
@@ -61,7 +69,8 @@ class GeoscriptService implements InitializingBean
           }
           break
         default:
-          if ( wfsParams[wfsParamName] ) {
+          if ( wfsParams[wfsParamName] )
+          {
             options[wfsParamName] = wfsParams[wfsParamName]
           }
         }
@@ -167,4 +176,227 @@ class GeoscriptService implements InitializingBean
     ( dataStore ) ? new Workspace( dataStore ) : null
   }
 
+  def getSchemaInfoByTypeName(String typeName)
+  {
+    def (prefix, layerName) = typeName?.split( ':' )
+
+    def layerInfo = LayerInfo.where {
+      name == layerName && workspaceInfo.namespaceInfo.prefix == prefix
+    }.get()
+
+    def workspaceInfo = layerInfo.workspaceInfo
+    def namespaceInfo = workspaceInfo.namespaceInfo
+
+    def schemaInfo = [
+        name: layerName,
+        namespace: [prefix: namespaceInfo.prefix, uri: namespaceInfo.uri],
+        schemaLocation: grailsLinkGenerator.serverBaseURL
+    ]
+
+    Workspace.withWorkspace( getWorkspace( workspaceInfo?.workspaceParams ) ) { workspace ->
+      def layer = workspace[layerName]
+      def schema = layer.schema
+
+      schemaInfo.attributes = layer.schema.fields.collect { field ->
+        def descr = schema.featureType.getDescriptor( field.name )
+        [
+            maxOccurs: descr.maxOccurs,
+            minOccurs: descr.minOccurs,
+            name: field.name,
+            nillable: descr.nillable,
+            type: field.typ
+        ]
+      }
+    }
+
+    schemaInfo
+  }
+
+  def getCapabilitiesData()
+  {
+    [
+        featureTypes: getLayerData(),
+        functionNames: listFunctions2(),
+        featureTypeNamespacesByPrefix: NamespaceInfo.list().inject( [:] ) { a, b ->
+          a[b.prefix] = b.uri; a
+        }
+
+    ]
+  }
+
+
+  def getLayerData()
+  {
+    LayerInfo.list()?.collect { layerInfo ->
+      def layerData
+      WorkspaceInfo workspaceInfo = WorkspaceInfo.findByName( layerInfo.workspaceInfo.name )
+      Workspace.withWorkspace( getWorkspace( workspaceInfo?.workspaceParams ) ) { Workspace workspace ->
+        def layer = workspace[layerInfo.name]
+        def uri = layer?.schema?.uri
+        def prefix = NamespaceInfo.findByUri( uri )?.prefix
+        def geoBounds
+
+        if ( layer.count() > 0 )
+        {
+          geoBounds = ( layer?.proj?.epsg == 4326 ) ? layer?.bounds : layer?.bounds?.reproject( 'epsg:4326' )
+        }
+        else
+        {
+          geoBounds = [minX: -180.0, minY: -90.0, maxX: 180.0, maxY: 90.0]
+        }
+
+        layerData = [
+            name: layerInfo.name,
+            namespace: [prefix: prefix, uri: uri],
+            title: layerInfo.title,
+            description: layerInfo.description,
+            keywords: layerInfo.keywords,
+            proj: layer.proj.id,
+            geoBounds: [minX: geoBounds.minX, minY: geoBounds.minY, maxX: geoBounds.maxX, maxY: geoBounds.maxY,]
+        ]
+
+      }
+
+      layerData
+    }
+  }
+
+
+  def getFeatureCsv(def wfsParams)
+  {
+    def layerInfo = findLayerInfo( wfsParams )
+    def result
+
+    def options = parseOptions( wfsParams )
+
+    def writer = new CsvWriter()
+    Workspace.withWorkspace( getWorkspace( layerInfo.workspaceInfo.workspaceParams ) ) {
+      workspace ->
+        def layer = workspace[layerInfo.name]
+        result = writer.write( layer.filter( wfsParams.filter ) )
+
+        workspace.close()
+    }
+
+
+    result
+  }
+
+  def getFeatureGML3(def wfsParams)
+  {
+    def layerInfo = findLayerInfo( wfsParams )
+    def xml
+
+    def options = parseOptions( wfsParams )
+    def workspaceParams = layerInfo?.workspaceInfo?.workspaceParams
+
+    //println "workspaceParams: ${workspaceParams}"
+
+    def x = {
+
+      Workspace.withWorkspace( getWorkspace( workspaceParams ) ) {
+        workspace ->
+          def layer = workspace[layerInfo.name]
+          def matched = layer?.count( wfsParams.filter ?: Filter.PASS )
+          def count = ( wfsParams.maxFeatures ) ? Math.min( matched, wfsParams.maxFeatures ) : matched
+          def namespaceInfo = layerInfo?.workspaceInfo?.namespaceInfo
+
+          def schemaLocations = [
+              namespaceInfo.uri,
+              grailsLinkGenerator.link( absolute: true, uri: '/wfs', params: [
+                  service: 'WFS',
+                  version: wfsParams.version,
+                  request: 'DescribeFeatureType',
+                  typeName: wfsParams.typeName
+              ] ),
+              "http://www.opengis.net/wfs",
+              grailsLinkGenerator.link( absolute: true, uri: '/schemas/wfs/1.1.0/wfs.xsd' )
+          ]
+
+          mkp.xmlDeclaration()
+          mkp.declareNamespace( ogcNamespacesByPrefix )
+          mkp.declareNamespace( "${namespaceInfo.prefix}": namespaceInfo.uri )
+
+          wfs.FeatureCollection(
+              numberOfFeatures: count,
+              timeStamp: new Date().format( "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", TimeZone.getTimeZone( 'GMT' ) ),
+              'xsi:schemaLocation': schemaLocations.join( ' ' ),
+              numberMatched: matched,
+              startIndex: wfsParams.startIndex ?: '0'
+          ) {
+            if ( !( wfsParams?.resultType?.toLowerCase() == 'hits' ) )
+            {
+              def features = layer?.getFeatures( options )
+
+              gml.featureMembers {
+                features?.each { feature ->
+                  mkp.yieldUnescaped(
+                      feature.getGml( version: 3, format: false, bounds: false, xmldecl: false, nsprefix: namespaceInfo.prefix )
+                  )
+                }
+              }
+            }
+          }
+      }
+    }
+
+    xml = new StreamingMarkupBuilder( encoding: 'utf-8' ).bind( x )
+
+    return xml.toString()
+  }
+
+  def getFeatureJSON(def wfsParams)
+  {
+    def layerInfo = findLayerInfo( wfsParams )
+    def results
+
+    def options = parseOptions( wfsParams )
+
+    Workspace.withWorkspace( getWorkspace( layerInfo.workspaceInfo.workspaceParams ) ) {
+      workspace ->
+        def layer = workspace[layerInfo.name]
+        def count = layer.count( wfsParams.filter ?: Filter.PASS )
+
+        def features = ( wfsParams.resultType != 'hits' ) ? layer.collectFromFeature( options ) { feature ->
+          return new JsonSlurper().parseText( feature.geoJSON )
+        } : []
+
+        results = [
+            crs: [
+                properties: [
+                    name: "urn:ogc:def:crs:${layer.proj.id}"
+                ],
+                type: "name"
+            ],
+            features: features,
+            totalFeatures: count,
+            type: "FeatureCollection"
+        ]
+
+        workspace.close()
+    }
+
+
+    return JsonOutput.toJson( results )
+  }
+
+  def getFeatureKML(def wfsParams)
+  {
+    def layerInfo = findLayerInfo( wfsParams )
+    def result
+
+    def options = parseOptions( wfsParams )
+
+    Workspace.withWorkspace( getWorkspace( layerInfo.workspaceInfo.workspaceParams ) ) {
+      workspace ->
+        def layer = workspace[layerInfo.name]
+        def features = layer.getFeatures( options )
+        result = kmlService.getFeaturesKml( features, [:] )
+
+        workspace.close()
+    }
+
+
+    result
+  }
 }
