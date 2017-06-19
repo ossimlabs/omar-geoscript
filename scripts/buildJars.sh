4f0d1b0d7e7ab0163ca8d969f40eda291a7132a4
Garrett Potts <gpotts@radiantblue.com>
2017-06-19 17:17:28 -0400

    Removed the buildJars.sh for that did not work either

--- scripts/buildJars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-pushd `dirname $0` >/dev/null
-SCRIPT_DIR=`pwd -P`
-pushd $SCRIPT_DIR/.. >/dev/null
-WORKDIR=$PWD
-popd > /dev/null
-popd >/dev/null
-gradle :omar-geoscript-app:artifactoryPublish
-gradle :omar-geoscript-plugin:artifactoryPublish
