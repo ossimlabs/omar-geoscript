569a4cf6b1b5c6fe5506ca7336a6c3718784d16c
Elangovan <pelangovan@radiantblue.com>
2017-10-11 17:21:58 -0400

    initial testing

--- apps/omar-geoscript-app/grails-app/services/geoscript/app/ElasticSearchService.groovy
+++ apps/omar-geoscript-app/grails-app/services/geoscript/app/ElasticSearchService.groovy
@@ -7,9 +7,16 @@ import org.springframework.beans.factory.annotation.Value
 
 class ElasticSearchService {
 
+    @Value('${geoscript.elasticsearch.host}')
     String searchHost
+
+    @Value('${geoscript.elasticsearch.port}')
     Integer hostPort
+
+    @Value('${geoscript.elasticsearch.index}')
     String indexName
+
+    @Value('${geoscript.elasticsearch.search}')
     String searchIndices
 
     ElasticDataStore  ES = new ElasticDataStore(searchHost, hostPort, indexName, searchIndices)
