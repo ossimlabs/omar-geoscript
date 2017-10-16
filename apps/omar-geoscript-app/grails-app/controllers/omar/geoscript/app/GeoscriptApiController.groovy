7ac01f4c3e1e1ac56e16c91d78ac29006d9b5a87
Aaron Nies <aaron.nies@digitalglobe.com>
2017-10-16 10:46:20 -0400

    Convert swagger notes from HTML to Markdown

--- apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/app/GeoscriptApiController.groovy
+++ apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/app/GeoscriptApiController.groovy
@@ -38,20 +38,13 @@ class GeoscriptApiController
   @ApiOperation(value = "Query a layer",
     produces='application/json',
         notes = """
-<style>
-    tab { padding-left: 2em; }
-</style>
-    <ul>
-        <li>
-            <b>sort</b> Is a comma separated field list of the form <br/>
-            <tab>&ltfield&gt &ltsort&gt</tab><br/></br>
-            <tab><b>Examples:</b></tab><br/>
-            <tab><tab><b>acquisition_date DESC<b></tab></tab><br/>
-            <tab><tab><b>acquisition_date ASC<b></tab></tab><br/>
-            <tab><tab><b>acquisition_date DESC,width ASC<b></tab></tab><br/>
-        </li>
-        <br/>
-    </ul>
+*   **sort** Is a comma separated field list of the form  
+    <tab><field> <sort></tab>  
+
+    <tab>**Examples:**</tab>  
+    <tab><tab>**acquisition_date DESC**</tab></tab>****  
+    <tab><tab>**acquisition_date ASC**</tab></tab>****  
+    <tab><tab>**acquisition_date DESC,width ASC**</tab></tab>********
 """)
   @ApiImplicitParams([
     @ApiImplicitParam(name = 'typeName', value = 'Type name', defaultValue="omar:raster_entry", paramType = 'query', dataType = 'string', required=true),
