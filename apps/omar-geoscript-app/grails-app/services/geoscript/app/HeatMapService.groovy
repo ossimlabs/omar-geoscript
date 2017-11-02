f7f1d521eadddca95ec7d25d9042836f818acd14
Praveen Elangovan <pelangovan@radiantblue.com>
2017-11-01 21:25:43 -0400

    testing...

--- apps/omar-geoscript-app/grails-app/services/geoscript/app/HeatMapService.groovy
+++ apps/omar-geoscript-app/grails-app/services/geoscript/app/HeatMapService.groovy
@@ -2,6 +2,36 @@ package geoscript.app
 
 import mil.nga.giat.data.elasticsearch.*
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.geotools.data.Query;
+import org.geotools.data.Transaction;
+import org.geotools.data.store.ContentDataStore;
+import org.geotools.data.store.ContentEntry;
+import org.geotools.data.store.ContentFeatureSource;
+import org.geotools.feature.NameImpl;
+import org.geotools.util.logging.Logging;
+import org.opengis.feature.type.Name;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+
+import mil.nga.giat.data.elasticsearch.ElasticAttribute.ElasticGeometryType;
+import mil.nga.giat.shaded.es.common.joda.Joda;
+
+
 import org.springframework.beans.factory.annotation.Value
 
 class HeatMapService {
@@ -26,7 +56,18 @@ class HeatMapService {
         println "indexname" + indexName
         println "searchindicies" + searchIndices
 
-        ElasticDataStore ES = new ElasticDataStore(searchHost, hostPort, indexName, searchIndices)
+        try {
+            final RestClient restClient = RestClient.builder(new HttpHost(searchHost, hostPort, "http")).build();
+            final Response response = restClient.performRequest("GET", "/", Collections.<String, String>emptyMap());
+            println "restClient" + restClient
+            println "code" + response.getStatusLine().getStatusCode()
+            if (response.getStatusLine().getStatusCode() >= 400) {
+                throw new IOException();
+            }
+            client = new RestElasticClient(restClient);
+        } catch (Exception e) {
+            throw new IOException("Unable to create REST client", e);
+        }
     }
 
 }
