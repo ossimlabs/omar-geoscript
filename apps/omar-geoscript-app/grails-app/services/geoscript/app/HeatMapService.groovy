a5d0e7950d3d68870723654bfd1881e6a380a3ab
Praveen Elangovan <pelangovan@radiantblue.com>
2017-11-03 12:45:18 -0400

    first try at ssl with code

--- apps/omar-geoscript-app/grails-app/services/geoscript/app/HeatMapService.groovy
+++ apps/omar-geoscript-app/grails-app/services/geoscript/app/HeatMapService.groovy
@@ -6,6 +6,34 @@ package geoscript.app
 //import org.apache.http.HttpHost;
 //import org.elasticsearch.client.Response;
 //import org.elasticsearch.client.RestClient;
+import static org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import javax.net.ssl.SSLContext
+import java.io.InputStream
+import java.security.KeyStore
+
 
 
 import org.springframework.beans.factory.annotation.Value
@@ -24,14 +52,30 @@ class HeatMapService {
     @Value('${geoscript.elasticsearch.search}')
     String searchIndices*/
 
-    def processHeatmap(String searchHost, Integer hostPort,
-                       String indexName, String searchIndices ) {
+    def processHeatmap() {
 
-        println "searchhsort" + searchHost
-        println "hostport" + hostPort
-        println "indexname" + indexName
-        println "searchindicies" + searchIndices
-        
+        def String KEYSTOREPATH = "/home/omar/es/admin.jks"; // or .p12
+        def String KEYSTOREPASS = "keystorepass";
+
+                InputStream keyStoreStream = this.getClass().getResourceAsStream(KEYSTOREPATH)
+                KeyStore keyStore = KeyStore.getInstance("JKS"); // or "PKCS12"
+                keyStore.load(keyStoreStream, null);
+
+        SSLContext sslContext = SSLContexts.custom()
+                    .loadKeyMaterial(keyStore, null) // use null as second param if you don't have a separate key password
+                    .build();
+
+            HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
+            HttpResponse response = httpClient.execute(new HttpGet("https://logging-es.logging.svc.cluster.local:9200/.all/_search?pretty"));
+            println
+             HttpEntity entity = response.getEntity();
+
+            System.out.println("----------------------------------------");
+            System.out.println(response.getStatusLine());
+            EntityUtils.consume(entity);
+        }
+       }
+
         /*try {
             println "got before rest client"
             final RestClient restClient = RestClient.builder(new HttpHost(searchHost, hostPort, "http")).build();
@@ -46,5 +90,3 @@ class HeatMapService {
         } catch (Exception e) {
             throw new IOException("Unable to create REST client", e);
         } */
-    }
-}
