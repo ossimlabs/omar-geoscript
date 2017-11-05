244c3f71cf6b0f8f6a819c19ffcb718afa8e04e6
Praveen Elangovan <pelangovan@radiantblue.com>
2017-11-05 08:02:02 -0500

    adding import

--- apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
+++ apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
@@ -28,6 +28,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy
+import org.apache.http.ssl.TrustSelfSignedStrategy
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
