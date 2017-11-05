74405d38bc8cf72ac05c352e34f61dbd0906f303
Praveen Elangovan <pelangovan@radiantblue.com>
2017-11-05 07:57:49 -0500

    trying trust strategy

--- apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
+++ apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
@@ -27,6 +27,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
