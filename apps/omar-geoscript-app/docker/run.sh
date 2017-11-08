f8394645a15dedd365aa10c0774921666dc1c0ef
Garrett Potts <gpotts@radiantblue.com>
2017-11-08 14:43:33 -0500

    Using JAVA_ARGS

--- apps/omar-geoscript-app/docker/run.sh
+++ apps/omar-geoscript-app/docker/run.sh
@@ -12,40 +12,40 @@
 #   export TRUST_STORE=${HOME}/es/truststore
 #fi
 
-if [ "${JAVA_PROPERTIES}" == "" ] ; then
-   export JAVA_PROPERTIES="-server -Xms256m -Xmx1024m -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -XX:+UnlockExperimentalVMOptions  -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Djava.security.egd=file:/dev/./urandom"
+if [ "${JAVA_ARGS}" == "" ] ; then
+   export JAVA_ARGS="-server -Xms256m -Xmx1024m -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -XX:+UnlockExperimentalVMOptions  -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Djava.security.egd=file:/dev/./urandom"
 fi
 
 if [ "${KEY_STORE}" != "" ] ; then
-   if [ "${JAVA_PROPERTIES}" == "" ] ; then
-      export JAVA_PROPERTIES="-Djavax.net.ssl.keyStore=${KEY_STORE}"
+   if [ "${JAVA_ARGS}" == "" ] ; then
+      export JAVA_ARGS="-Djavax.net.ssl.keyStore=${KEY_STORE}"
    else
-      export JAVA_PROPERTIES="-Djavax.net.ssl.keyStore=${KEY_STORE} ${JAVA_PROPERTIES}"
+      export JAVA_ARGS="-Djavax.net.ssl.keyStore=${KEY_STORE} ${JAVA_ARGS}"
    fi
 fi 
 
 if [ "${KEY_STORE_PASSWORD}" != "" ] ; then
-   if [ "${JAVA_PROPERTIES}" == "" ] ; then
-      export JAVA_PROPERTIES="-Djavax.net.ssl.keyStorePassword=${KEY_STORE_PASSWORD}"
+   if [ "${JAVA_ARGS}" == "" ] ; then
+      export JAVA_ARGS="-Djavax.net.ssl.keyStorePassword=${KEY_STORE_PASSWORD}"
    else
-      export JAVA_PROPERTIES="-Djavax.net.ssl.keyStorePassword=${KEY_STORE_PASSWORD} ${JAVA_PROPERTIES}"
+      export JAVA_ARGS="-Djavax.net.ssl.keyStorePassword=${KEY_STORE_PASSWORD} ${JAVA_ARGS}"
    fi
 fi 
 
 if [ "${TRUST_STORE}" != "" ] ; then
-   if [ "${JAVA_PROPERTIES}" == "" ] ; then
-      export JAVA_PROPERTIES="-Djavax.net.ssl.trustStore=${TRUST_STORE}"
+   if [ "${JAVA_ARGS}" == "" ] ; then
+      export JAVA_ARGS="-Djavax.net.ssl.trustStore=${TRUST_STORE}"
    else
-      export JAVA_PROPERTIES="-Djavax.net.ssl.trustStore=${TRUST_STORE} ${JAVA_PROPERTIES}"
+      export JAVA_ARGS="-Djavax.net.ssl.trustStore=${TRUST_STORE} ${JAVA_ARGS}"
    fi
 fi 
 
 if [ "${TRUST_STORE_PASSWORD}" != "" ] ; then
-   if [ "${JAVA_PROPERTIES}" == "" ] ; then
-      export JAVA_PROPERTIES="-Djavax.net.ssl.trustStorePassword=${TRUST_STORE_PASSWORD}"
+   if [ "${JAVA_ARGS}" == "" ] ; then
+      export JAVA_ARGS="-Djavax.net.ssl.trustStorePassword=${TRUST_STORE_PASSWORD}"
    else
-      export JAVA_PROPERTIES="-Djavax.net.ssl.trustStorePassword${TRUST_STORE_PASSWORD} ${JAVA_PROPERTIES}"
+      export JAVA_ARGS="-Djavax.net.ssl.trustStorePassword${TRUST_STORE_PASSWORD} ${JAVA_ARGS}"
    fi
 fi 
 
-java $JAVA_PROPERTIES -jar *.jar
+java ${JAVA_ARGS} -jar *.jar
