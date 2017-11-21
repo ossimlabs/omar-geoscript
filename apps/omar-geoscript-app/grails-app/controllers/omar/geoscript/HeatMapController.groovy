b0f2f3c78d3282a1909fd73b7f61bed97bee8600
Garrett Potts <gpotts@radiantblue.com>
2017-11-21 11:33:53 -0500

    Added binding from core

--- apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/HeatMapController.groovy
+++ apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/HeatMapController.groovy
@@ -2,6 +2,7 @@ package omar.geoscript
 
 import org.springframework.beans.factory.annotation.Value
 import omar.geoscript.WmsRequest
+import omar.core.BindUtil
 
 class HeatMapController {
 
@@ -14,8 +15,10 @@ class HeatMapController {
 
 	def getTile(WmsRequest wmsRequest)
 	{
-	println params
-	println wmsRequest
+		println params
+		println wmsRequest
+		BindUtil.fixParamNames( WmsRequest, params )
+	   bindData( wmsRequest, params )
 
 		def results = heatMapService.getTile( wmsRequest, elasticSearchURL )
 
