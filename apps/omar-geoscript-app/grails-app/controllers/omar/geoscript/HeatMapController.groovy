4abbad6101ed5c1bbecc88c480d5601edb5dd32b
Praveen Elangovan <pelangovan@radiantblue.com>
2017-11-21 16:09:18 -0500

    added bbox error checks

--- apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/HeatMapController.groovy
+++ apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/HeatMapController.groovy
@@ -17,17 +17,39 @@ class HeatMapController {
 	{
 		println "params" + params
 		println "wmsrequest" + wmsRequest
-
-		println "got before fixParamNames"
 		BindUtil.fixParamNames( WmsRequest, params )
-		println "got before bindData"
 	   bindData( wmsRequest, params )
 
 		// error checking code
+		def bbox_int = new int[4]
+		def process = 1
 
-		println "got before get tile"
-		def results = heatMapService.getTile( wmsRequest, elasticSearchURL )
+		arrayList = wms.bbox.split(",")
+		arrayList.length
+		if(arrayList.length != 4)
+		{
+			log.error "bbox does not have valid numbers"
+			process = 0
+		}
 
-		render contentType: results.contentType, file: results.buffer
+		for(int i = 0;i<4;i++)
+			bbox_int[i] = Integer.valueOf(arrayList[i])
+
+		if(bbox_int[0] < -180 || bbox_int[3] > 180 || bbox_int[1] < -90 || bbox_int[3] > 90) {
+			log.error "bbox out of bounds"
+            process = 0
+		}
+
+		if((bbox_int[2] <= bbox_int[0]) || (bbox_int[3] <= bbox_int[1])) {
+			log.error "bbox range is wrong"
+            process = 0
+		}
+
+
+		if(process == 1) {
+			def results = heatMapService.getTile(wmsRequest, elasticSearchURL)
+
+			render contentType: results.contentType, file: results.buffer
+		}
 	}
 }
