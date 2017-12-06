131fc188ad24f8454dd61e01e6ccbfa897e407d4
Praveen Elangovan <pelangovan@radiantblue.com>
2017-12-06 14:50:00 -0500

    catching exception

--- apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
+++ apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
@@ -85,40 +85,44 @@ class HeatMapService {
             {
                 if((isValidJson(result.hits.hits.getAt(i)._source.message))) {
                     Feature feature = writer.newFeature
-                    Map<String, Object> logmap = new ObjectMapper().readValue(result.hits.hits.getAt(i)._source.message, HashMap.class);
+                    try {
+                        Map<String, Object> logmap = new ObjectMapper().readValue(result.hits.hits.getAt(i)._source.message, HashMap.class);
+
+                        String timestamplog = logmap.timestamp
+                        log.info "\ntimestamp" + logmap.timestamp
+                        DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.ms", Locale.ENGLISH);
+                        Date date = format.parse(timestamplog);
+
+                        def currenttime = new Date()
+
+                        timediff = Math.abs(currenttime.getTime() - date.getTime())
+                        // log.info "message" + result.hits.hits.getAt(i)._source.message
+
+                        if(timediff <= (days*60*60*24*1000)) {
 
 
-                    String timestamplog = logmap.timestamp
-                    log.info "\ntimestamp" + logmap.timestamp
-                    DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.ms", Locale.ENGLISH);
-                    Date date = format.parse(timestamplog);
-
-                    def currenttime = new Date()
-
-                    timediff = Math.abs(currenttime.getTime() - date.getTime())
-                    // log.info "message" + result.hits.hits.getAt(i)._source.message
-
-                    if(timediff <= (days*60*60*24*1000)) {
+                            Point centroid = new Point((logmap.bbox.minX +
+                                    logmap.bbox.maxX) / 2.0,
+                                    (logmap.bbox.minY +
+                                            logmap.bbox.maxY) / 2.0)
 
 
-                        Point centroid = new Point((logmap.bbox.minX +
-                                logmap.bbox.maxX) / 2.0,
-                                (logmap.bbox.minY +
-                                        logmap.bbox.maxY) / 2.0)
+                            Projection proj = projectionMap."${logmap.bbox.proj.id}"
+                            if (!proj) {
+                                proj = new Projection(logmap.bbox.proj.id)
+                                projectionMap."${logmap.bbox.proj.id}" = proj
+                            }
+                            Point targetPoint = proj.transform(centroid, targetProjection) as Point
 
 
-                        Projection proj = projectionMap."${logmap.bbox.proj.id}"
-                        if (!proj) {
-                            proj = new Projection(logmap.bbox.proj.id)
-                            projectionMap."${logmap.bbox.proj.id}" = proj
+                            feature.set([
+                                    geom: targetPoint
+                            ])
+                            writer.add(feature)
                         }
-                        Point targetPoint = proj.transform(centroid, targetProjection) as Point
-
-
-                        feature.set([
-                                geom: targetPoint
-                        ])
-                        writer.add(feature)
+                    }
+                    catch (Exception e) {
+                        log.error "Can't parse json"
                     }
                 }
             }
