e1a73008234a4da62feeec9c106706b909e7afdd
Praveen Elangovan <pelangovan@radiantblue.com>
2017-12-06 10:55:02 -0500

    sending back heatmap with a given number of days

--- apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
+++ apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
@@ -24,21 +24,24 @@ class HeatMapService {
         }
     }
 
-    URL buildQueryUrl(String wmsStartDate, String wmsEndDate, String esUrl) {
-        // esUrl = https://logging-es.logging.svc.cluster.local:9200/project.omar-dev*/
+/*    URL buildQueryUrl(String wmsStartDate, String wmsEndDate, String esUrl) {
+         esUrl = https://logging-es.logging.svc.cluster.local:9200/project.omar-dev
         String urlSearchParam = "_search?"
-//        String timeStampFormat = "yyyy-MM-dd hh:mm:ss.ms"
         String timeStampFormat = "yyyy-MM-dd hh:mm:ss.ms"
-//        String query = """{"query":{"range":{"timestamp":{"gte":"${wmsStartDate}","lte":"${wmsEndDate}","format":"${timeStampFormat}"}},"term":{"kubernetes.labels.deploymentconfig":"omar-wms-app"}}}"""
+        String timeStampFormat = "yyyy-MM-dd hh:mm:ss.ms"
+        String query = """{"query":{"range":{"timestamp":{"gte":"${wmsStartDate}","lte":"${wmsEndDate}","format":"${timeStampFormat}"}},"term":{"kubernetes.labels.deploymentconfig":"omar-wms-app"}}}"""
 
-//        String query = """{"query":{"range":{"@timestamp":{"gte":"2017-11-30T05:48:42.809770+00:00","lte": "2017-11-30T18:06:38.779477+00:00"}}}}"""
+        String query = """{"query":{"range":{"@timestamp":{"gte":"2017-11-30T05:48:42.809770+00:00","lte": "2017-11-30T18:06:38.779477+00:00"}}}}"""
         String query = """%7b"query":%7b"bool":%7b"must":%5b%7b"range":%7b"@timestamp":%7b"gt":"2017-11-28T14:04:21+0000"%2c"lt":"2017-11-30T14:04:21+0000"%7d%7d%7d%5d%2c"must_not":%5b%5d%2c"should":%5b%5d%7d%7d%2c"from":0%2c"size":10%2c"sort":%5b%5d%2c%7d"""
 
-//        (esUrl + urlSearchParam +  URLEncoder.encode(query, "UTF-8")).toURL()
+        (esUrl + urlSearchParam +  URLEncoder.encode(query, "UTF-8")).toURL()
         (esUrl + urlSearchParam +  query ).toURL()
-    }
+    }*/
 
     Layer getLayer(WmsRequest wmsRequest, String req) {
+
+        def days = 1
+
         Workspace workspace = new Memory()
         Schema schema = new Schema("heatmap", [
                 new Field("geom", "Point", wmsRequest.srs)
@@ -46,11 +49,11 @@ class HeatMapService {
         Layer layer = workspace.create(schema)
         log.info "wmsRequest.start_date" + wmsRequest.start_date
         log.info "wmsRequest.end_date" + wmsRequest.end_date
-        log.info "req" + req
 
-
-        URL url = buildQueryUrl(wmsRequest.start_date, wmsRequest.end_date, req)
+//        URL url = buildQueryUrl(wmsRequest.start_date, wmsRequest.end_date, req)
+        URL url = new URL(req);
         log.info("ES Search URL: " + url)
+
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection()
         InputStream is = conn.getInputStream()
@@ -70,25 +73,39 @@ class HeatMapService {
                     Feature feature = writer.newFeature
                     Map<String, Object> logmap = new ObjectMapper().readValue(result.hits.hits.getAt(i)._source.message, HashMap.class);
 
+                    String timestamplog = logmap.timestamp
+                    log.info "timestamp" + timestamplog
+                    DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.ms", Locale.ENGLISH);
+                    Date date = format.parse(timestamplog);
+                    log.info "date" + date
 
-                    Point centroid = new Point((logmap.bbox.minX +
-                            logmap.bbox.maxX) / 2.0,
-                            (logmap.bbox.minY +
-                                    logmap.bbox.maxY) / 2.0)
+                    def currenttime = new Date()
+
+                    timediff = Math.abs(currenttime.getTime() - date.getTime())
+                    log.info "currtime" + currenttime.getTime()
+                    log.info "logtime" + date.getTime()
+                    log.info "timediff" + timediff
+
+                    if(timediff <= (days*60*60*24*1000)) {
+                        Point centroid = new Point((logmap.bbox.minX +
+                                logmap.bbox.maxX) / 2.0,
+                                (logmap.bbox.minY +
+                                        logmap.bbox.maxY) / 2.0)
 
 
-                    Projection proj = projectionMap."${logmap.bbox.proj.id}"
-                    if (!proj) {
-                        proj = new Projection(logmap.bbox.proj.id)
-                        projectionMap."${logmap.bbox.proj.id}" = proj
+                        Projection proj = projectionMap."${logmap.bbox.proj.id}"
+                        if (!proj) {
+                            proj = new Projection(logmap.bbox.proj.id)
+                            projectionMap."${logmap.bbox.proj.id}" = proj
+                        }
+                        Point targetPoint = proj.transform(centroid, targetProjection) as Point
+
+
+                        feature.set([
+                                geom: targetPoint
+                        ])
+                        writer.add(feature)
                     }
-                    Point targetPoint = proj.transform(centroid, targetProjection) as Point
-
-
-                    feature.set([
-                            geom: targetPoint
-                    ])
-                    writer.add(feature)
                 }
             }
         }
