244d46a46088487afa5926f591fa2a5ab4ff17b9
Praveen Elangovan <pelangovan@radiantblue.com>
2017-12-14 13:12:55 -0500

    added correct query

--- apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
+++ apps/omar-geoscript-app/grails-app/services/omar/geoscript/HeatMapService.groovy
@@ -65,7 +65,7 @@ class HeatMapService {
         Layer layer = workspace.create(schema)
 
         URL url = new URL(req);
-        String x = """{"from":0, "size":10000, "query":{"range":{"@timestamp":{"gte": "${wmsRequest.start_date}","lte":"${wmsRequest.end_date}"}}}}"""
+        String x = """{"from":0, "size":10000, "query":{"bool": { "must": [ { "term": {"requestMethod": "GetMap"}}, {"range":{"@timestamp":{"gte": "${start_date}","lte":"${end_date}"}}}]}}}"""
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setDoOutput(true);
