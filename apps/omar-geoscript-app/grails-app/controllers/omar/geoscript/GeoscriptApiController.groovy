d2e8f37a11a059e4ea037aeab46cf311aca95f8f
Stephen L'Allier <stephen.lallier@digitalglobe.com>
2018-02-26 17:33:56 -0600

    revert class name changes

--- apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/GeoscriptApiController.groovy
+++ apps/omar-geoscript-app/grails-app/controllers/omar/geoscript/GeoscriptApiController.groovy
@@ -7,7 +7,7 @@ import io.swagger.annotations.*
 @Api(value = "/geoscriptApi",
      description = "GeoScript Support"
 )
-class GeoscriptController
+class GeoscriptApiController
 {
   def geoscriptService
 
