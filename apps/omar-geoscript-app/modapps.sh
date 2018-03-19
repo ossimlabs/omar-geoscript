777219589b980e84ee3f3ce976716f7850cc183a
knaquin <kevin.naquin@digitalglobe.com>
2018-03-19 12:20:24 -0400

    Using gradle for builds.

--- apps/omar-geoscript-app/modapps.sh
+++ apps/omar-geoscript-app/modapps.sh
@@ -6,7 +6,7 @@ DOCKER_REGISTRY_URL=docker-registry-default.o2.radiantbluecloud.com
 REGISTRY_PROJECT_NAME=o2
 OPENSHIFT_URL=https://openshift-master.radiantbluecloud.com:8443
 
-./gradlew assemble
+gradle assemble
 docker build -t $APP_NAME:$APP_TAG .
 docker tag $APP_NAME:latest $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG
 oc login $OPENSHIFT_URL
