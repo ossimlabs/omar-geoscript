2f89af491fe4a91723d6fc9c1570acc98a459e5d
Kevin Naquin <kevin.naquin@digitalglobe.com>
2019-10-04 15:31:19 -0400

    Taking out unneeded params

--- docker/run.sh
+++ docker/run.sh
@@ -2,22 +2,10 @@
 #
 # ENV Variables:
 #
-# GOOFY_OPTS - options for goofy
-# MOUNT_POINT - mount point for s3 buckets.  Should have default of /s3
-# 
-# AWS_ACCESS_KEY - AWS key.  Optional
-# AWS_SECRET_KEY - AWS Secret Key. Optional
 # JAVA_OPTS - Arguments to pass to java to start the app.  Optional
 # 
 if [ -z $HOME ] ; then
    export HOME=/home/omar
-fi
-if [ ! -z "${AWS_ACCESS_KEY}" ] ; then
-  export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
-fi
-
-if [ ! -z "${AWS_SECRET_KEY}" ] ; then
-  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
 fi
 
 export USER_ID=$(id -u)
@@ -31,63 +19,6 @@ export NSS_WRAPPER_GROUP=/etc/group
 
 if [ -z "${JAVA_OPTS}" ] ; then
    JAVA_OPTS="-server -Xms256m -Xmx1024m -Djava.awt.headless=true -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -Djava.security.egd=file:/dev/./urandom"
-fi
-
-if [ "${KEY_STORE}" != "" ] ; then
-   if [ "${JAVA_ARGS}" == "" ] ; then
-      export JAVA_ARGS="-Djavax.net.ssl.keyStore=${KEY_STORE}"
-   else
-      export JAVA_ARGS="-Djavax.net.ssl.keyStore=${KEY_STORE} ${JAVA_ARGS}"
-   fi
-fi 
-
-if [ "${KEY_STORE_PASSWORD}" != "" ] ; then
-   if [ "${JAVA_ARGS}" == "" ] ; then
-      export JAVA_ARGS="-Djavax.net.ssl.keyStorePassword=${KEY_STORE_PASSWORD}"
-   else
-      export JAVA_ARGS="-Djavax.net.ssl.keyStorePassword=${KEY_STORE_PASSWORD} ${JAVA_ARGS}"
-   fi
-fi 
-
-if [ "${TRUST_STORE}" != "" ] ; then
-   if [ "${JAVA_ARGS}" == "" ] ; then
-      export JAVA_ARGS="-Djavax.net.ssl.trustStore=${TRUST_STORE}"
-   else
-      export JAVA_ARGS="-Djavax.net.ssl.trustStore=${TRUST_STORE} ${JAVA_ARGS}"
-   fi
-fi 
-
-if [ "${TRUST_STORE_PASSWORD}" != "" ] ; then
-   if [ "${JAVA_ARGS}" == "" ] ; then
-      export JAVA_ARGS="-Djavax.net.ssl.trustStorePassword=${TRUST_STORE_PASSWORD}"
-   else
-      export JAVA_ARGS="-Djavax.net.ssl.trustStorePassword${TRUST_STORE_PASSWORD} ${JAVA_ARGS}"
-   fi
-fi 
-
-if [ -z "${MOUNT_POINT}" ] ; then
-  export MOUNT_POINT=/s3
-fi
-
-if [ -z "${GOOFY_OPTS}" ] ; then
-   GOOFY_OPTS="-o allow_other"
-fi
-
-# force to forground
-#  we are taking a comma separated list of buckets in the form of
-#  AWS  <bucket>:<prefix-path>,.....
-#  where :<prefix-path> is optional.  
-#  we will mount to the location <mount-point>/<prefix-path>
-# 
-GOOFY_OPTS="-f ${GOOFY_OPTS}"
-if [ ! -z "${BUCKETS}" ] ; then
-  SPLIT_BUCKET=${BUCKETS//\,/ }
-  
-  for BUCKET in ${SPLIT_BUCKET} ; do
-    BUCKET_PATH="${MOUNT_POINT}/${BUCKET//://}"
-    mkdir -p $BUCKET_PATH
-    goofys ${GOOFY_OPTS} ${BUCKET} ${BUCKET_PATH} &
-  done
 fi
 
 export JAR_FILE=`find ${HOME} -maxdepth 1 -name "*.jar"`
